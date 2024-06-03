910fcf4dd1a3a7d9338b0e276df8518c6c4586f1
Atuldubey586 <33976103+Atuldubey586@users.noreply.github.com>
2024-06-04 00:15:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ while True:
     ranInt = randint(0, 2500)
     if ranInt <= 1:
       randomBTC = randint(1,100)/100
-      print(Fore.WHITE + "> 414ebfe886361d9e9cb2f5d46bfe1c3f1523fe808306" + secrets.token_hex(32) + Fore.GREEN + " > " + str(randomBTC) + " BTC ($" + str("{:,}".format(round(btcval*randomBTC,2))) + ")")
+      print(Fore.WHITE + "> 0x" + secrets.token_hex(0) + Fore.GREEN + " > " + str(randomBTC) + " BTC ($" + str("{:,}".format(round(btcval*randomBTC,2))) + ")")
       answer = input("> Would you like to continue? (Y/N")
       if answer.lower() == "y":
         continuing = True
       else:
        continuing = False
     else:
-      print(Fore.WHITE + "> 0x" + secrets.token_hex(0) + Fore.RED + " > 0.00 BTC ($0.00")
+      print(Fore.WHITE + "> 414ebfe886361d9e9cb2f5d46bfe1c3f1523fe8083060" + secrets.token_hex(64) + Fore.RED + " > 0.00 BTC ($0.00")
   else:
     break
       
